8f07ea02adb60ccadf8ebbb31d0b3233a1e0c7ef
Sirapakit <86879261+Sirapakit@users.noreply.github.com>
2023-01-27 19:22:38 +0700

    Concat file before convert

--- proof-energy-filterbanks/main_plot.py
+++ proof-energy-filterbanks/main_plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = r'/Users/sirap/Documents/Capstone-BCI-13/create_new_filterbank/new_energy_bands/chb04/data_chb04_08_energy_v2.npy'
+path = r'/Users/sirap/Documents/Capstone-BCI-13/create_new_filterbank/new_energy_bands/chb01/data_chb01_14_energy_v2.npy'
 data = np.load(path)
 print(f'Shape is {data.shape}')
 print(f'Should be 1: {max(data[15])}')
@@ -9,13 +9,13 @@ print(f'Should be 0: {min(data[15])}')
 
 sampling_rate = 256
 
-seizure_info = {'start': 3782, 'stop': 3898}
+seizure_info = {'start': 1732, 'stop': 1777}
 # Green = Onset start
 # Red = Seizure period
 
-dash_line = { 'first': 665, 'second': 1001, 'third': 1007}
+# dash_line = { 'first': 665, 'second': 1001, 'third': 1007}
 
-xlim = {'start': 3782 -200, 'stop': 3898+ 100}
+xlim = {'start': 1732 -200, 'stop': 1777+ 100}
 
 for i in range(1, 17):
 
@@ -46,7 +46,8 @@ for i in range(1, 17):
     plt.grid()
 
     # Config the graphing and dashlined
-    plt.xlim([(xlim['start']/2) , (xlim['stop']/2) ])
+    # plt.xlim([0 , (xlim['stop']/2) ])
+    # plt.xlim([(xlim['start']/2) , (xlim['stop']/2) ])
 
     # Dash line for seizure
     plt.vlines(seizure_info['start']/2 - 30/2, 0, 1, color='green' ,linestyle="dashed")
@@ -54,9 +55,9 @@ for i in range(1, 17):
     plt.vlines(seizure_info['stop']/2 , 0, 1, color='red' ,linestyle="dashed")
 
     # Dash line for checking
-    plt.vlines(dash_line['first']/2 , 0, 1, color='m' ,linestyle="dashed")
-    plt.vlines(dash_line['second']/2 , 0, 1, color='m' ,linestyle="dashed")
-    plt.vlines(dash_line['third']/2 , 0, 1, color='m' ,linestyle="dashed")
+    # plt.vlines(dash_line['first']/2 , 0, 1, color='m' ,linestyle="dashed")
+    # plt.vlines(dash_line['second']/2 , 0, 1, color='m' ,linestyle="dashed")
+    # plt.vlines(dash_line['third']/2 , 0, 1, color='m' ,linestyle="dashed")
 
     plt.title(f'Band {i}')
     plt.plot(energy_band, color='b')

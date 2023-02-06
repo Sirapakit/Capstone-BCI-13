71423c4c10b5cc16ca8762d00eee4d6ca2e98df3
Cat Savinee <savinee.khr@student.mahidol.edu>
2023-02-06 16:28:27 +0700

    shift-window

--- lsl/demo_inlet.py
+++ lsl/demo_inlet.py
@@ -6,49 +6,73 @@ import pylsl
 import pyqtgraph as pg
 from typing import List
 from pylsl import StreamInlet, resolve_stream
-# from joblib import load
 
-def filter(sample_from_lsl):
-    result = []
-    for i in sample_from_lsl:
-        result.append(i*2)
-    return result
 
-def main():
-    # first resolve an EEG stream on the lab network
-    print("looking for an EEG stream...")
-    streams = resolve_stream()
-    print(f'streams(20): {streams}')
-    print(streams)
+SAMPLING_FREQ = 256
+WINDOW_LENGTH = 8
+OVERLAP_PERCENT = 0.5
+SHIFTED_DURATION = 4
 
-    # create a new inlet to read from the stream
-    inlet_Fp2_F8 = StreamInlet(streams[0])
-    inlet_F8_T8 = StreamInlet(streams[1])
-    info = pylsl.StreamInfo()
-    # streams = pylsl.resolve_streams()
-    # print(f'streams(26): {streams}')
+class Test():
+    
+    Fp2_F8_data = np.array([0, 1, 2, 3, 4, 5, 6, 7],dtype='float64')  # 8 = SAMPLING_FREQ * WINDOW_LENGTH
+    F8_T8_data = np.array([0, 1, 2, 3, 4, 5, 6, 7],dtype='float64')
 
-    sample_Fp2_F8 = []
-    sample_F8_T8 = []
-    count = 0
+    def __init__(self,sample_Fp2_F8,sample_F8_T8):
+        self.sample_Fp2_F8 = sample_Fp2_F8
+        self.sample_F8_T8 = sample_F8_T8
 
-    while count<10: 
-        # get a new sample (you can also omit the timestamp part if you're not
-        # interested in it)
-        for info in streams:
-            sample_in_Fp2_F8, timestamps = inlet_Fp2_F8.pull_sample()
-            # print('Adding marker inlet: ' + info.name())
-            sample_Fp2_F8.append(sample_in_Fp2_F8[0])
-            sample_in_F8_T8, timestamps = inlet_F8_T8.pull_sample()
-            # print('Adding marker inlet: ' + info.name())
-            sample_F8_T8.append(sample_in_F8_T8[0])
-        time.sleep(0.0004)
-        count += 1
-    print(f'sample0: {sample_Fp2_F8}')
-    print(f'sample1: {sample_F8_T8}')
+    def main(self):
+        self.sample_Fp2_F8 = np.array([])
+        self.sample_F8_T8 = np.array([])
+        # first resolve an EEG stream on the lab network
+        streams = resolve_stream()
+
+
+        # create a new inlet to read from the stream
+        inlet_Fp2_F8 = StreamInlet(streams[0])
+        inlet_F8_T8 = StreamInlet(streams[1])
+        info = pylsl.StreamInfo()
+
+        count = 0
+
+        while count<2: 
+            # get a new sample (you can also omit the timestamp part if you're not
+            # interested in it)
+            for info in streams:
+                self.sample_in_Fp2_F8, timestamps = inlet_Fp2_F8.pull_sample()
+                self.sample_Fp2_F8 = np.hstack((self.sample_Fp2_F8,np.round(self.sample_in_Fp2_F8[0],7)))
+    
+                self.sample_in_F8_T8, timestamps = inlet_F8_T8.pull_sample()
+                self.sample_F8_T8 = np.hstack((self.sample_F8_T8,np.round(self.sample_in_F8_T8[0],7)))
+
+            time.sleep(0.0004)
+            count += 1
+        print(f'sample0: {self.sample_Fp2_F8}')
+        print(f'sample1: {self.sample_F8_T8}')
+
+        return self.sample_Fp2_F8,self.sample_F8_T8
+
+    def data_slicing(self):
+        
+        self.Fp2_F8_roll = np.roll(self.Fp2_F8_data, SHIFTED_DURATION)
+        self.F8_T8_roll = np.roll(self.F8_T8_data, SHIFTED_DURATION)
+
+        self.Fp2_F8_roll[-SHIFTED_DURATION:] = self.sample_Fp2_F8
+        self.F8_T8_roll[-SHIFTED_DURATION:] = self.sample_F8_T8
+
+        self.Fp2_F8_data = self.Fp2_F8_roll
+        self.F8_T8_data = self.F8_T8_roll
+
+        print(self.Fp2_F8_data)
+        print(self.F8_T8_data)
+
 
 if __name__ == '__main__':
-    main()
+    a = Test(np.array([]),np.array([]))   # new 4 sec data each channel
+    while True:
+        a.main()
+        a.data_slicing()
 
 
 
